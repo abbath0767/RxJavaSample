17d76092d1b353883b40fd714b6f6da3fe4f42c9
Abbath0767 <ng@redmadrobot.com>
2016-11-22 18:24:40 +0300

    need upgrade

--- src/main/PartFour/Main.java
+++ src/main/PartFour/Main.java
@@ -1,10 +1,6 @@
 package PartFour;
 
 import rx.Observable;
-import rx.Subscriber;
-
-import java.util.Arrays;
-import java.util.Random;
 
 public class Main {
 
@@ -12,54 +8,55 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        ProcessFactory.generateProcessing(5)
-//                .flatMap(processes -> Observable.from(processes))
-//                .flatMap(process -> Observable.just(process))
-//                .doOnNext(process -> {
-//                    process.start();
-//                })
-//                .subscribe(process -> System.out.println(process.toString() + " subscribed"));
+        ProcessFactory.generateProcessing(5)
+                .flatMap(processes -> Observable.from(processes))
+                .flatMap(process -> {
+                    process.start();
+                    return Observable.just(process);
+                })
+//                .doOnNext(process -> process.start())
+                .subscribe(process -> System.out.println(process.toString() + " subscribed"));
+
+        System.out.println("<><><><><><><><><><>");
+
+
+//        Observable<Process> observable = Observable.create(new Observable.OnSubscribe<Process>() {
+//            @Override
+//            public void call(Subscriber<? super Process> subscriber) {
+//                Random rand = new Random();
+//                int bound = 4;
+//                Process p = Process.newProcess("P" + counter++, rand.nextInt(bound));
+//                subscriber.onNext(p);
+//                try {
+//                    p.join();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("COMPLETE");
+//                System.out.println("data: " + Arrays.toString(p.getData()));
+//                subscriber.onCompleted();
+//            }
+//        });
+////
+//        Subscriber<Process> subscriber = new Subscriber<Process>() {
+//            @Override
+//            public void onCompleted() {
+//                System.out.println("onComplited");
+//            }
 //
-//        System.out.println("<><><><><><><><><><>");
-
-
-        Observable<Process> observable = Observable.create(new Observable.OnSubscribe<Process>() {
-            @Override
-            public void call(Subscriber<? super Process> subscriber) {
-                Random rand = new Random();
-                int bound = 4;
-                Process p = Process.newProcess("P" + counter++, rand.nextInt(bound));
-                subscriber.onNext(p);
-                try {
-                    p.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("COMPLETE");
-                System.out.println("data: " + Arrays.toString(p.getData()));
-                subscriber.onCompleted();
-            }
-        });
+//            @Override
+//            public void onError(Throwable throwable) {
+//                System.out.println("error: " + throwable.toString());
+//            }
 //
-        Subscriber<Process> subscriber = new Subscriber<Process>() {
-            @Override
-            public void onCompleted() {
-                System.out.println("onComplited");
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println("error: " + throwable.toString());
-            }
-
-            @Override
-            public void onNext(Process process) {
-                System.out.println("onNext: " + process.toString());
-                process.start();
-            }
-        };
-
-        observable.subscribe(subscriber);
+//            @Override
+//            public void onNext(Process process) {
+//                System.out.println("onNext: " + process.toString());
+//                process.start();
+//            }
+//        };
+//
+//        observable.subscribe(subscriber);
 
 
     }
